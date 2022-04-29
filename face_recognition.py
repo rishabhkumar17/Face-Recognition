7237528db40bd38b388ba420df1525d7b48f8484
Rishabh <rishabhkaushal1796@gmail.com>
2022-04-29 23:01:36 +0530

    Built Face Classifier

--- face_recognition.py
+++ face_recognition.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 import os
 
-def dist(v1,v2):
+def distance(v1,v2):
 
     return np.sqrt(sum((v1-v2)**2)) # Euclidean distance
 
@@ -13,9 +13,8 @@ def knn(train,test,k=5):
     for i in range(train.shape[0]):
 
         ix = train[i, :-1]
-        iy = train[i, :-1]
-
-        d  = dist(test, ix)
+        iy = train[i, -1]
+        d = distance(test, ix)
         dist.append([d, iy])
         
     dk = sorted(dist, key=lambda x: x[0])[:k]
@@ -23,7 +22,6 @@ def knn(train,test,k=5):
     labels = np.array(dk)[:, -1]
     
     output = np.unique(labels, return_counts = True)
-    print(new_vals)
     
     index = np.argmax(output[1])
     return output[0][index]
@@ -45,7 +43,7 @@ names = {} #Mapping between id and name
 # Data Preperation
 for fx in os.listdir(dataset_path):
     if fx.endswith('.npy'):
-
+        names[class_id] = fx[:-4]
         print("Loaded "+fx)
         data_item = np.load(dataset_path+fx)
         face_data.append(data_item)
@@ -56,7 +54,45 @@ for fx in os.listdir(dataset_path):
         labels.append(target)
 
 face_dataset = np.concatenate((face_data),axis=0)
-face_labels = np.concatenate((labels),axis=0)
+face_labels = np.concatenate((labels),axis=0).reshape((-1,1))
 
 print(face_dataset.shape)
 print(face_labels.shape)
+
+#train
+trainset = np.concatenate((face_dataset,face_labels),axis=1)
+print(trainset.shape)
+
+#Testing
+
+while True:
+    ret,frame = cap.read()
+    if ret == False:
+        continue
+
+    faces = face_cascade.detectMultiScale(frame,1.3,5)
+
+    for face in faces:
+        x,y,w,h = face
+
+        #Get the face ROI
+        offset = 10
+        face_section = frame[y-offset:y+h+offset,x-offset:x+w+offset]
+        face_section = cv2.resize(face_section,(100,100))
+
+        out = knn(trainset,face_section.flatten())
+
+        #display
+        pred_name = names[int(out)]
+        cv2.putText(frame,pred_name,(x,y-10),cv2.FONT_HERSHEY_SIMPLEX,1,(255,0,0),2,cv2.LINE_AA)
+        cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,255),2)
+
+
+    cv2.imshow("Faces",frame)
+
+    key = cv2.waitKey(1) & 0xFF
+    if key == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
